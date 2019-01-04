4136b2ce834a3d36fe78c0d6d2f65829c0210052
Steven <steve.chung96@yahoo.com>
2019-01-04 00:26:47 -0800

    Personal Website Upload

---
+++ interact.js
@@ -0,0 +1,19 @@
+function goHome() {
+    url = "file:///Users/stevenchung/Desktop/HTML%20Project/index.html#home";
+   window.location=url;
+}
+
+function goIG() {
+    url = "https://www.instagram.com/steven_daniel96/?hl=en";
+    window.open(url);
+}
+
+function goFB() {
+    url = "https://www.facebook.com/steven.chung.9041";
+    window.open(url);
+}
+
+function goResume() {
+    url = "Resume.pdf";
+    window.open(url);
+}
